5f92742f5d5ed53d80668f7be28639864dcb2b9b
Alicetel-u <Alicetel-u@users.noreply.github.com>
2026-08-21 15:09:05 +0900

    Use Ruby photo app icon

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='bunny-stamps-v1';
+const CACHE='bunny-stamps-v2';
 const ASSETS=['./','./index.html','./style.css','./app.js','./app-icon.png','./ruby-stamps-sheet.png','./manifest.webmanifest'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS))));
 self.addEventListener('fetch',event=>{if(event.request.method==='GET'&&new URL(event.request.url).origin===location.origin)event.respondWith(caches.match(event.request).then(saved=>saved||fetch(event.request)))});
